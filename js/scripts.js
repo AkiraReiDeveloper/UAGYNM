a0b85f2671260585e79acb26a07c663dc6c78990
AkiraReiDeveloper <45138556+AkiraReiDeveloper@users.noreply.github.com>
2022-04-26 20:04:31 -0600

    update

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,14 @@ function teacherinfo(id) {
         lang.className = "main__teacher__info--hide";
     }
 }
+
+function consejoinfo(id) {
+    var lang = document.getElementById("consejo__hide_"+id);
+
+    if (lang.className === "container__consejo--info--hide") {
+        lang.className = "container__consejo--info--show";
+        console.log(lang.className);
+    } else {
+        lang.className = "container__consejo--info--hide";
+    }
+}
